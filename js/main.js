d41ae8e077c57394e755c8b6772c978079f5d499
mohamedelaraby <mohamedelaraby1995@gmail.com>
2018-11-28 02:00:12 -0800

    modifing project stracture

---
+++ js/main.js
@@ -0,0 +1,22 @@
+ Volume in drive C is O S
+ Volume Serial Number is B87B-03AA
+
+ Directory of C:\Users\DaRa$$\Desktop\web-design
+
+11/24/2018  03:34 AM    <DIR>          .
+11/24/2018  03:34 AM    <DIR>          ..
+11/20/2018  12:47 PM         2,111,673 air-arm-autumn-762041.jpg
+11/20/2018  12:47 PM         2,005,053 architecture-billboards-bridge-1130287.jpg
+11/24/2018  03:21 AM         1,928,673 backlit-beach-blur-940880.jpg
+11/21/2018  03:21 PM           320,442 beach-blur-clouds-462030.jpg
+11/20/2018  12:46 PM         2,483,901 beach-cliffs-evening-977736.jpg
+11/24/2018  03:20 AM           588,013 beach-dark-dock-943516.jpg
+11/24/2018  03:20 AM         5,615,754 cars-dubai-fast-1245077.jpg
+11/24/2018  03:21 AM         3,551,045 cold-frosty-frozen-1417651.jpg
+11/24/2018  03:21 AM           572,832 evening-highway-light-streaks-1375569.jpg
+11/20/2018  12:47 PM           373,187 green-hand-holding-1605400.jpg
+11/24/2018  03:34 AM             1,072 index.html
+11/24/2018  03:34 AM                 0 main.js
+11/24/2018  03:34 AM             1,122 style.css
+              13 File(s)     19,552,767 bytes
+               2 Dir(s)  181,880,668,160 bytes free
